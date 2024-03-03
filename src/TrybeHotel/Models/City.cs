8ac59ae728b82dfae3f450725ca088a0e33e5412
Pamela-Cas <pxcx611@gmail.com>
2024-03-03 18:38:03 -0300

    criando model city

--- src/TrybeHotel/Models/City.cs
+++ src/TrybeHotel/Models/City.cs
@@ -5,6 +5,10 @@ namespace TrybeHotel.Models
 
     // 1. Implemente as models da aplicação
     public class City {
+        [Key]
+        public int CityId { get; set; }
+        public string? Name { get; set; }
+
+        public virtual ICollection<Hotel>? Hotels { get; set; }}
 
     }
-}
