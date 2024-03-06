bf0ff6a6599b500c84ab84331a1035a4b9a9849e
Pamela-Cas <pxcx611@gmail.com>
2024-03-06 11:04:10 -0300

    concluindo dto

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -7,15 +7,14 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) {
         Seeder.SeedUserAdmin(this);
     }
-    public TrybeHotelContext() { }
     public DbSet<City> Cities { get; set; }
     public DbSet<Hotel> Hotels { get; set; }
     public DbSet<Room> Rooms { get; set; }
-    public DbSet<User> Users { get; set; }
     public DbSet<Booking> Bookings { get; set; }
+    public DbSet<User> Users { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-        var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+        var connectionString = "Server=localhost;Database=TrybeHotel;User=SA; Password=TrybeHotel12!; Trust Server Certificate=true";
 
         optionsBuilder.UseSqlServer(connectionString);
     }
