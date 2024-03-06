35ee4947d4a70d50d40c1e2caeea7b2c07b2c4ab
Pamela-Cas <pxcx611@gmail.com>
2024-03-06 11:11:13 -0300

    criando models

--- src/TrybeHotel/Models/City.cs
+++ src/TrybeHotel/Models/City.cs
@@ -5,10 +5,9 @@ namespace TrybeHotel.Models
 
     // 1. Implemente as models da aplicação
     public class City {
-        [Key]
         public int CityId { get; set; }
         public string? Name { get; set; }
-
-        public virtual ICollection<Hotel>? Hotels { get; set; }}
+        public List<Hotel>? Hotels { get; set; }
+      }
 
     }
--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -4,12 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models  da aplicação
 public class Hotel {
-  [Key]
-  public int HotelId { get; set; }
-  public string? Name { get; set; }
-  public string? Address { get; set; }
-  [ForeignKey("CityId")]
-  public int CityId { get; set; }
-  public virtual ICollection<Room>? Rooms { get; set; }
+    public int HotelId { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+    public int CityId { get; set; }
+    public City? City { get; set; }
+    public List<Room>? Rooms { get; set; }
 
 }
